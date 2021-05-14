70f62a64a4f784c2fcc6e037f7ab629d6101c5e1
losk773 <losk773@gmail.com>
2021-05-15 00:25:58 +0300

    add weatherService

---
+++ src/services/weatherService.ts
@@ -0,0 +1,19 @@
+import { tempUnits } from '../constant/tempUnits';
+import apiService from './apiService';
+
+const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
+
+export const WeatherService = {
+  getGeoByLocationName(name: string) {
+    return apiService.get(`/geo/1.0/direct?q=${name}&appid=${apiKey}`);;
+  },
+  getForecastWeather(lat: number, lon: number, unit: string) {
+    return apiService.get(`/data/2.5/onecall?lat=${lat}&lon=${lon}&units=${unit}&exclude=minutely,hourly,alerts&appid=${apiKey}`);
+  },
+  getAirPollution(lat: number, lon: number) {
+    return apiService.get(`/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+  },
+  getWeatherIconURL(code: string) {
+    return `${process.env.REACT_APP_WEATHER_IMG_URL}/${code}@2x.png`;
+  } 
+}
