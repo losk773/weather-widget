429a40c3d6bc79b45510ceea7f9490d510c62164
losk773 <losk773@gmail.com>
2021-05-15 00:26:22 +0300

    add weather types

---
+++ src/features/weather/weatherTypes.ts
@@ -0,0 +1,69 @@
+export type TWeather = {
+  description: string;
+  icon: string;
+}
+
+export type TCurrentWeather = {
+  humidity: number;
+  temp: number;
+  dt: number;
+  weather: TWeather[];
+  wind_deg: number;
+  wind_speed: number;
+}
+
+export type TWeatherDay = {
+  dt: number;
+  temp: {
+    max: number,
+    min: number
+  };
+  weather: TWeather[];
+  humidity: number;
+  wind_speed: number;
+  wind_deg: number;
+}
+
+export type TWeatherState = {
+  data: {
+    location: string;
+    timezoone: string;
+    current: TCurrentWeather;
+    daily: TWeatherDay[];
+  } | null;
+  loading: boolean;
+  error: string| null;
+  unit: string;
+}
+
+export enum WeatherActionTypes {
+  WEATHER_FORECAST_REQUEST = 'WEATHER_FORECAST_REQUEST',
+  WEATHER_FORECAST_SUCCESS = 'WEATHER_FORECAST_SUCCESS',
+  WEATHER_FORECAST_ERROR = 'WEATHER_FORECAST_ERROR',
+  SET_WEATHER_UNIT = 'SET_WEATHER_UNIT',
+}
+
+type TWeatherForecastRequest = {
+  type: WeatherActionTypes.WEATHER_FORECAST_REQUEST;
+}
+
+type TWeatherForecastSuccess = {
+  type: WeatherActionTypes.WEATHER_FORECAST_SUCCESS;
+  payload: {};
+}
+
+type TWeatherForecastError = {
+  type: WeatherActionTypes.WEATHER_FORECAST_ERROR;
+  payload: string;
+}
+
+type TWeatherUnit = {
+  type: WeatherActionTypes.SET_WEATHER_UNIT;
+  payload: string;
+}
+
+export type TWeatherAction = 
+  TWeatherForecastRequest | 
+  TWeatherForecastSuccess | 
+  TWeatherForecastError |
+  TWeatherUnit;
