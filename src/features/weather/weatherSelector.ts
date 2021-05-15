bc122b19a9b6704daff8fd8c9ffdc1ff8499ba43
losk773 <losk773@gmail.com>
2021-05-15 03:32:42 +0300

    add new weather selectors

--- src/features/weather/weatherSelector.ts
+++ src/features/weather/weatherSelector.ts
@@ -3,12 +3,33 @@ import { TCurrentWeather, TWeatherState } from './weatherTypes';
 
 export const getWeather = (state: RootState): TWeatherState => state.weather;
 
+export const getWeatherByIndex = (state: RootState, index: number): TCurrentWeather | null => {
+  if (index === 0 && state.weather.data) {
+    return state.weather.data.current;
+  }
+  
+  const weather = state.weather.data?.daily[index];
+
+  return weather ? {
+    ...weather,
+    temp: weather.temp.max,
+  } : null;
+};
+
 export const getWeatherTimezone = (state: RootState): string => {
   return state.weather.data ? state.weather.data.timezone : ''
 };
 
 export const getWeatherLocation = (state: RootState): string => {
   return state.weather.data ? state.weather.data.location : ''
+};
+
+export const getWeatherAir = (state: RootState): number => {
+  return state.weather.data ? state.weather.data.air : 1;
+};
+
+export const getSelectedWeatherDay = (state: RootState): number => {
+  return state.weather.selected;
 };
 
 export const getCurrentWeather = (state: RootState): TCurrentWeather | null => {
